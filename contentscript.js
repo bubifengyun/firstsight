4bf0ae426ae06fcd1384d00bfb8d001c113caa2f
arthurchiao <arthurchiao@hotmail.com>
2017-11-23 22:33:18 +0800

    add modal, but not all types of pages work

--- contentscript.js
+++ contentscript.js
@@ -2,28 +2,32 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     console.log("received request: %s", request);
     switch (request.type){
         case "openModal":
-            alert(request.type);
-            var iframe  = document.createElement ('iframe');
-            iframe.src  = chrome.extension.getURL ('modal.html');
-            document.body.appendChild (iframe);
-        // var imgURL = chrome.extension.getURL("modal.html");
-        // chrome.windows.create(imgURL, null);
-            // chrome.tabs.create({
-            //     url: chrome.extension.getURL('modal.html'),
-            //     active: false
-            // }, function(tab) {
-            //     // After the tab has been created, open a window to inject the tab
-            //     chrome.windows.create({
-            //         tabId: tab.id,
-            //         type: 'popup',
-            //         focused: true
-            //             // incognito, top, left, ...
-            //     });
-            // });
-            // $('#myModal').modal({
-            //     backdrop: 'static',
-            //     keyboard: false
-            // });
+            $.get(chrome.extension.getURL('modal.html'), function(data) {
+                $(data).appendTo('body');
+                // Or if you're using jQuery 1.8+:
+                // $($.parseHTML(data)).appendTo('body');
+
+				// // Get the modal
+				var modal = document.getElementById('myModal');
+
+				// // Get the <span> element that closes the modal
+				var span = document.getElementsByClassName("close")[0];
+
+				modal.style.display = "block";
+
+				// // When the user clicks on <span> (x), close the modal
+				span.onclick = function() {
+					modal.style.display = "none";
+				}
+
+				// When the user clicks anywhere outside of the modal, close it
+				// window.onclick = function(event) {
+				// 	if (event.target == modal) {
+				// 		modal.style.display = "none";
+				// 	}
+				// }
+
+            });
             break;  
-    }
+	}
     });
